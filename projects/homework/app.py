97d4399d64f2c522fd612e7a07db209fd12a5247
bae_giweon <rlwi440@naver.com>
2020-08-25 22:38:46 +0900

    [update] alone practice API project Review Again

--- projects/homework/app.py
+++ projects/homework/app.py
@@ -17,30 +17,29 @@ def homework():
 # 주문하기(POST) API
 @app.route('/order', methods=['POST'])
 def save_order():
-    # name_receive로 클라이언트가 준 name  가져오기
     name_receive = request.form['name_give']
     item_receive = request.form['item_give']
-    address_receive =request.form['address_give']
-    number_receive =request.form['number_give']
+    address_receive = request.form['address_give']
+    number_receive = request.form['number_give']
 
-    # DB에 삽입할 review 만들기
     doc = {
-            'name': name_receive,
-            'item': item_receive,
-            'address': address_receive,
-            'number' : number_receive
-            }
-    # orders에 doc 저장하기
+        'name': name_receive,
+        'item': item_receive,
+        'address': address_receive,
+        'number': number_receive
+    }
+    # reviews에 review 저장하기
     db.orders.insert_one(doc)
-    # 성공 여부 & 성공 메시지 반환
-    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
+    return jsonify({'result': 'success', 'msg': ''})
 
 
 # 주문 목록보기(Read) API
 @app.route('/order', methods=['GET'])
 def view_orders():
-    oders = list(db.orders.find({}, {'_id': False}))
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+    orders = list(db.orders.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'orders': orders})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
