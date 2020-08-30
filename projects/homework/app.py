9600b029a96beee03db9940dd4f666f9a68d1135
bae_giweon <rlwi440@naver.com>
2020-08-30 14:52:26 +0900

    [update] first project title 'loopy' completion app.py

--- projects/homework/app.py
+++ projects/homework/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
 db = client.dbhomework
 
 
